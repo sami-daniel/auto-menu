4b0de1d229b2edf96ca560d4105d922b67848d47
Sami Daniel <samidaniel125@gmail.com>
2024-04-28 19:20:03 -0300

    Classe para teste unitario do servico do restaurante

---
+++ XUnitServicoTeste/RestauranteServicoTeste.cs
@@ -0,0 +1,12 @@
+﻿using Servicos.Abstracao;
+using Servicos.DTO.AddRequests;
+using Servicos.Implementacoes;
+using Xunit.Sdk;
+
+namespace XUnitServicoTeste
+{
+    public class RestauranteServicoTeste
+    {
+        
+    }
+}
