0f2db41e9d6e19a73b5802d879610c43802d3ba8
Sami Daniel <samidaniel125@gmail.com>
2024-04-28 19:12:54 -0300

    Criando a camada de servico do restaurante que implementa sua abstracao

---
+++ Servicos/Implementacoes/RestauranteServico.cs
@@ -0,0 +1,19 @@
+﻿using Servicos.Abstracao;
+using Servicos.DTO.AddRequests;
+using Servicos.DTO.Responses;
+
+namespace Servicos.Implementacoes
+{
+    public class RestauranteServico : IRestauranteServico
+    {
+        public Task<RestauranteResponse> AddRestauranteAsync(RestauranteAddRequest restauranteAddRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<RestauranteResponse> GetAllRestaurantes()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
