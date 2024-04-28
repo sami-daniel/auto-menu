51206ea48382d1f8c713a5c0239614bdc8207046
Sami Daniel <samidaniel125@gmail.com>
2024-04-28 19:34:44 -0300

    Criada a excessao para quando o restaurante ja exisitr

---
+++ Servicos/Exceptions/RestauranteExistenteException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Servicos.Exceptions
+{
+    public class RestauranteExistenteException : Exception
+    {
+        public RestauranteExistenteException(string message = "O restaurante já existe no sistema") : base(message)
+        {
+        }
+    }
+}
