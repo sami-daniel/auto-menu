42017038bb480330d2b49afe50d236c6564a9c2e
Sami Daniel <samidaniel125@gmail.com>
2024-04-28 19:28:09 -0300

    Mudanca na mensagem de erro da senha do restauranteAddRequest

--- Servicos/DTO/AddRequests/RestauranteAddRequest.cs
+++ Servicos/DTO/AddRequests/RestauranteAddRequest.cs
@@ -42,7 +42,7 @@ namespace Servicos.DTO.AddRequests
         [Required(ErrorMessage = "A senha é obrigatória.")]
         [StringLength(30, MinimumLength = 8, ErrorMessage = "A senha deve ter entre 8 e 30 caracteres.")]
         [Display(Name = "Senha")]
-        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*?&])[A-Za-z@$!%*?&]*$", ErrorMessage = "A senha deve conter pelo menos uma letra minúscula, uma letra maiúscula, um caractere especial (@$!%*?&) e pode conter apenas letras, números e os seguintes caracteres especiais: @$!%*?&")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*?&])[A-Za-z@$!%*?&]*$", ErrorMessage = "A senha deve conter pelo menos uma letra minúscula, uma letra maiúscula, um caractere especial (@$!%*?&)")]
         public string Senha { get; set; }
 
 
