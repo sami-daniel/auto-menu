9c9a9e60c4f11d4035fb64b0eae01d6edb0f3607
Sami Daniel <samidaniel125@gmail.com>
2024-04-28 10:41:24 -0300

    EnderecoServico criado. Testes unitario devem ser feitos no servico antes da sua implementação de fato

---
+++ Servicos/Implementacoes/EnderecoServico.cs
@@ -0,0 +1,14 @@
+﻿using Servicos.Abstracao;
+using Servicos.DTO.AddRequests;
+using Servicos.DTO.Responses;
+
+namespace Servicos.Implementacoes
+{
+    public class EnderecoServico : IEnderecoServico
+    {
+        public EnderecoResponse AddEndereco(EnderecoAddRequest enderecoAddRequest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
