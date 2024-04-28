5f1857d159e9d17e86acff736e2914686f49b13e
Pedro Ramos <145812059+PedroRamosdev@users.noreply.github.com>
2024-04-27 23:11:32 -0300

    rota raiz deletada

--- AutoMenu/Controllers/HomeController.cs
+++ AutoMenu/Controllers/HomeController.cs
@@ -5,7 +5,6 @@ namespace project_name.Controllers
     //Controlador HOME
     public class HomeController : Controller
     {
-        [Route("/")]
         public IActionResult Index()
         {
             // metodo IActionResult para a pagina index principal
