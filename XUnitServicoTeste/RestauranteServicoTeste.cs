64695c80afd1f6a0e65167d387f0c1402d7a38d2
Sami Daniel <samidaniel125@gmail.com>
2024-04-28 19:20:40 -0300

    Teste unitario para RestauranteAddRequestNulo

--- XUnitServicoTeste/RestauranteServicoTeste.cs
+++ XUnitServicoTeste/RestauranteServicoTeste.cs
@@ -7,6 +7,24 @@ namespace XUnitServicoTeste
 {
     public class RestauranteServicoTeste
     {
+        private readonly IRestauranteServico _restauranteServico;
+        public RestauranteServicoTeste()
+        {
+            _restauranteServico = new RestauranteServico();
+        }
+        [Fact]
+        // O restaurante a ser adicionado nao pode ser nulo
+        public async Task AddRestauranteAsync_RestauranteAddRequestNulo()
+        {
+            //Arrange
+            RestauranteAddRequest? restauranteAddRequest = null;
+            //Assert
+            await Assert.ThrowsAsync<ArgumentNullException>(async () =>
+            {
+                //Act
+                await _restauranteServico.AddRestauranteAsync(restauranteAddRequest!);
+            });
+        }
         
     }
 }
