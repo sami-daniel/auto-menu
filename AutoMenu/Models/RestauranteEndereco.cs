a0b928a2d415f412adf7655b88cbfb63b00d12c3
Sami Daniel <samidaniel125@gmail.com>
2024-04-28 20:46:33 -0300

    tornando o modelo publico

--- AutoMenu/Models/RestauranteEndereco.cs
+++ AutoMenu/Models/RestauranteEndereco.cs
@@ -1,11 +1,11 @@
 ﻿using Servicos.DTO.AddRequests;
 
-namespace project_name.Models
+namespace AutoMenu.Models
 {
     /// <summary>
     /// Modelo para tornar a view de cadastro fortemente tipada para usar taghelpers
     /// </summary>
-    internal class RestauranteEndereco
+    public class RestauranteEndereco
     {
         public RestauranteAddRequest RestauranteAddRequest { get; } = new RestauranteAddRequest();
         public EnderecoAddRequest EnderecoAddRequest { get; } = new EnderecoAddRequest();
