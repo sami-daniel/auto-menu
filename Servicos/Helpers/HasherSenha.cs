28c92698a98ffcaf1e8f7ceab8fd1186169f7798
Sami Daniel <samidaniel125@gmail.com>
2024-04-27 21:25:01 -0300

    Hasher de senha para encriptar e verificar senhas

---
+++ Servicos/Helpers/HasherSenha.cs
@@ -0,0 +1,8 @@
+﻿namespace Servicos.Helpers
+{
+    public static class HasherSenha
+    {
+        public static string HashearSenha(string senha) => BCrypt.Net.BCrypt.HashPassword(senha);
+        public static bool VerificarSenha(string inputSenha, string senhaHasheada) => BCrypt.Net.BCrypt.Verify(inputSenha, senhaHasheada);
+    }
+}
