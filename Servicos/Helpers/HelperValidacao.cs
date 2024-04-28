33e175b55b6efdb8c14c28312a2f42fccc20be7f
Sami Daniel <samidaniel125@gmail.com>
2024-04-28 15:37:43 -0300

    Helper para ajudar a validaar um objeto

---
+++ Servicos/Helpers/HelperValidacao.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Servicos.Helpers
+{
+    public static class HelperValidacao
+    {
+        public static bool IsValido(object obj)
+        {
+            ValidationContext validationContext = new ValidationContext(obj);
+
+            List<ValidationResult> validationResults = new List<ValidationResult>();
+
+            bool isValid = Validator.TryValidateObject(obj, validationContext, validationResults);
+
+            if(!isValid) 
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
