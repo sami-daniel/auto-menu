1b425218e02a6f847fb1c8acfbb78697feeb6b44
Sami Daniel <samidaniel125@gmail.com>
2024-04-28 20:39:53 -0300

    modelo RestauranteEndereco para armazenar exemplo de restauranteAddRequest e EnderecoAddRequest

---
+++ AutoMenu/Models/RestauranteEndereco.cs
@@ -0,0 +1,13 @@
+﻿using Servicos.DTO.AddRequests;
+
+namespace project_name.Models
+{
+    /// <summary>
+    /// Modelo para tornar a view de cadastro fortemente tipada para usar taghelpers
+    /// </summary>
+    internal class RestauranteEndereco
+    {
+        public RestauranteAddRequest RestauranteAddRequest { get; } = new RestauranteAddRequest();
+        public EnderecoAddRequest EnderecoAddRequest { get; } = new EnderecoAddRequest();
+    }
+}
