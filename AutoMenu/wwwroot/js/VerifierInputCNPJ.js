91938a5657b6daefee77d59585697b50f1ae2003
Sami Daniel <samidaniel125@gmail.com>
2024-05-05 13:35:18 -0300

    Script para envio de requisicao para determinar se o CNPJ é valido ou nao

---
+++ AutoMenu/wwwroot/js/VerifierInputCNPJ.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+    $('#Cnpj').keyup(function () {
+        var inputCnpj = $(this).val();
+
+        $.ajax({
+            url: '/Account/CheckCNPJAvailability', // Verifique se a rota está correta
+            method: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(inputCnpj), // Apenas envie a string diretamente
+            success: (response) => {
+                if (response === 'Invalid CNPJ') {
+                    $('#error-Cnpj').text('CNPJ Inválido'); // Use .text() para definir o texto do elemento
+                }
+                else {
+                    $('#error-Cnpj').text('CNPJ Válido');
+                }
+            },
+            error: (xhr, status, error) => {
+                $('#error-Cnpj').text('CNPJ Inválido');
+            }
+        });
+    });
+});
