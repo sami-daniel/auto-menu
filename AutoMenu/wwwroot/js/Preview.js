aad68376d0d8501093c102401b38a2166dc5f9dc
Pedro Ramos <145812059+PedroRamosdev@users.noreply.github.com>
2024-05-06 00:29:59 -0300

    Cadastro itens html feito

---
+++ AutoMenu/wwwroot/js/Preview.js
@@ -0,0 +1,17 @@
+const fileInput = document.getElementById('img');
+  const preview = document.getElementById('preview');
+
+  fileInput.addEventListener('change', function() {
+    const file = this.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = function(e) {
+        preview.src = e.target.result;
+        preview.style.display = 'block';
+      }
+      reader.readAsDataURL(file);
+    } else {
+      preview.src = '#';
+      preview.style.display = 'none';
+    }
+  });
