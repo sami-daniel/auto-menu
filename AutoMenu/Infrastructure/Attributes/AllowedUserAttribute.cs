dcf9196f6576854ae574fa3cf9e21c4221747b8e
Sami Daniel <samidaniel125@gmail.com>
2024-05-11 15:25:20 -0300

    AttributeUsage definido na classe

--- AutoMenu/Infrastructure/Attributes/AllowedUserAttribute.cs
+++ AutoMenu/Infrastructure/Attributes/AllowedUserAttribute.cs
@@ -1,5 +1,6 @@
 ﻿namespace AutoMenu.Infrastructure.Attributes
 {
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
     public class AllowedUserAttribute : Attribute
     {
     }
