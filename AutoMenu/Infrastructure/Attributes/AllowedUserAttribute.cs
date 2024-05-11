8be66dfa43d3b966529105e12bfdcb3ff86d931e
Sami Daniel <samidaniel125@gmail.com>
2024-05-11 16:12:46 -0300

    mano eu fiquei com tanta preguiça de commitar isso aqui

--- AutoMenu/Infrastructure/Attributes/AllowedUserAttribute.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace AutoMenu.Infrastructure.Attributes
-{
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
-    public class AllowedUserAttribute : Attribute
-    {
-    }
-}
---
+++ AutoMenu/Infrastructure/Convetions/ApplyCheckSessionObjectFilterConvention.cs
@@ -0,0 +1,18 @@
+﻿using AutoMenu.Infrastructure.Filters;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+
+namespace AutoMenu.Infrastructure.Convetions
+{
+    public class ApplyCheckSessionObjectFilterConvention : IApplicationModelConvention
+    {
+        public void Apply(ApplicationModel application)
+        {
+            var controllerName = application.Controllers.First(c => c.ControllerName == "Interface").ControllerType.Name;
+
+            if (controllerName == "InterfaceController")
+            {
+                application.Filters.Add(new CheckSessionObjectFilter());
+            }
+        }
+    }
+}
